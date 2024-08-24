470e711dd8ef5e5362e9e6074d0598394294fc6e
PlamenCode <plamenggg233@gmail.com>
2024-08-25 01:09:25 +0300

    basic page layout and game rules

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Tic-Tac-Toe-2
+ tic tac toe game without draws ;)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1>Wellcome to Tic-Tac-Toe-2</h1>
+        <div className='tic-tac-toe'>
+        <div className="rules">
+            <h2>THE RULES !!</h2>
+            <p>It starts as the regular game</p>
+            <p>BUT players can have only 3 boxes</p>
+            <p>ticked When you have 3 boxes the next </p>
+            <p>time you play your first box will</p>
+            <p>get empty and so on UNTIL</p>
+            <p>someone gets 3 on a.</p>
+            <p>SIMPLE ENOUGH RIGHT??</p>
+            <p></p>
+        </div>
+        <div className="game">
+            
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
